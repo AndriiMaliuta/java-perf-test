aaa48bc620e698491ee1fe28552d4884b6a7f4b9
andrii <quadr988@gmail.com>
2022-04-23 14:47:32 +0300

    performance

--- src/main/java/com/anma/http/HttpExe.java
+++ src/main/java/com/anma/http/HttpExe.java
@@ -12,7 +12,7 @@ public class HttpExe {
 
     public static void testHttp() {
         var url = "https://marketplace.atlassian.com/rest/2/vendors/1216206/reporting/sales/transactions?limit=30";
-        var tokenStr = String.format("quadr988@gmail.com:%s", System.getenv("ATLAS_TOKEN"));
+        var tokenStr = System.getenv("QUADR_MAIL") + ":"  + System.getenv("ATLAS_TOKEN");
         var token = Base64.getEncoder().encode(tokenStr.getBytes());
         String header1 = String.format("Basic %s", new String(token));
         System.out.println(header1);
